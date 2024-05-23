45c9edba4abcea026c7f6be0ae4e1569c79c95cd
Raphaël Duchaîne <duchainer@gmail.com>
2024-05-23 13:12:00 -0400

    Followed page 3 - pre assignment
    Following https://academy.cosmwasm.com/learn/smart-contracts/creating-a-message

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-use cosmwasm_std::{DepsMut, Env, MessageInfo, Empty, StdResult, Response, entry_point, Deps, QueryResponse};
+use cosmwasm_std::{
+    Deps, DepsMut, Empty, entry_point, Env, MessageInfo, QueryResponse, Response, StdResult,
+    to_json_binary,
+};
 
+use crate::msg::QueryMsg;
+
+mod contract;
+pub mod msg;
 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty
-)-> StdResult<Response>{
-    Ok(Response::new())
-}
-
-#[entry_point]
-pub fn execute(
     _deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
@@ -22,10 +19,16 @@ pub fn execute(
 }
 
 #[entry_point]
-pub fn query(
-    _deps: Deps,
-    _env: Env,
-    _msg: Empty,
-) -> StdResult<QueryResponse> {
-    Ok(QueryResponse::new(vec![]))
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[entry_point]
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<QueryResponse> {
+    use contract::query;
+    use msg::QueryMsg::Value;
+
+    match msg {
+        Value {} => to_json_binary(&query::value()),
+    }
 }
